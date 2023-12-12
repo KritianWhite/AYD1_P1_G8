04f857ab0bb330f0d68341a13e3b957d336877b7
Angelgt3 <69768799+Angelgt3@users.noreply.github.com>
2023-12-11 22:47:18 -0600

    correcion ruta eliminar libro

--- Backend/src/controllers/libroController.ts
+++ Backend/src/controllers/libroController.ts
@@ -215,71 +215,71 @@ class LibroController{
     // post - Escribir comentario a un libro 
     public async IngresarComentario(req: Request, res: Response): Promise<void> {
         try {
-            const email  = corregirFormato(req.params.email);
-            const titulo  = corregirFormato(req.params.titulo);
-            const { comentario } = req.body.comentario;
-            var idUsuario,idLibro;
+            const email = corregirFormato(req.params.email);
+            const titulo = corregirFormato(req.params.titulo);
+            const { comentario } = req.body;
             // Obtener id_usuario e id_libro
             pool.query('SELECT id_usuario, id_libro FROM USUARIO, LIBRO WHERE email = ? AND titulo = ?',
             [email, titulo], (error, results) => {
-                // Verifica si hay resultados 
-                if (!(results.length == 0)) {
-                    idUsuario = results[0].id_usuario;
-                    idLibro = results[0].id_libro;
-                }else {
+                // Verifica si hay resultados
+                if (results.length > 0) {
+                    const idUsuario = results[0].id_usuario;
+                    const idLibro = results[0].id_libro;
+                    // Insertar comentario
+                    pool.query('INSERT INTO COMENTARIO (comentario, usuario, libro) VALUES (?, ?, ?)',
+                        [comentario, idUsuario, idLibro], (insertError) => {
+                            if (insertError) {
+                                console.error('Error al insertar comentario:', insertError);
+                                res.status(500).json({ message: 'Error al insertar comentario' });
+                            } else {
+                                res.json({ message: 'Comentario agregado' });
+                            }
+                        });
+                } else {
                     res.status(404).json({ message: 'Usuario o libro no encontrado' });
-                    return;
                 }
             });
-            // Insertar comentario
-            pool.query('INSERT INTO COMENTARIO (comentario, usuario, libro) VALUES (?, ?, ?)',
-            [comentario, idUsuario, idLibro], (insertError) => {
-                res.json({ message: 'Comentario agregado' })
-            });
-            
         } catch (error) {
-            console.error('Error en rentar el libro:', error);
-            res.status(500).json({ message: 'Error en rentar el libro' });
+            console.error('Error en ingresar comentario:', error);
+            res.status(500).json({ message: 'Error en ingresar comentario' });
         }
     }
 
      // POST - Eliminar comentario -tiene que ser el autor del comentario
-    public EliminarComentario(req: Request, res: Response): void {
+     public EliminarComentario(req: Request, res: Response): void {
         try {
-            const email  = corregirFormato(req.params.email);
-            const { comentarioId } = req.body;
-
-            // Obtener id_usuario del usuario que realiza la solicitud
-            pool.query('SELECT id_usuario FROM USUARIO WHERE email = ?', [email], (error, results) => {
-
-                if (results.length === 0) {
-                    res.status(404).json({ message: 'Usuario no encontrado' });
-                    return;
-                }
-                const idUsuarioSolicitante = results[0].id_usuario;
-                // Verificar si el usuario es el autor del comentario
-                pool.query('SELECT id_usuario FROM COMENTARIO WHERE id_comentario = ?', [comentarioId], (error, results) => {
-                    if (results.length === 0) {
-                        res.status(404).json({ message: 'Comentario no encontrado' });
-                        return;
-                    }
-                    const idUsuarioAutor = results[0].id_usuario;
-                    if (idUsuarioSolicitante !== idUsuarioAutor) {
-                        res.status(403).json({ message: 'No tienes permiso para eliminar este comentario' });
-                        return;
-                    }
-                    // Eliminar el comentario
-                    pool.query('DELETE FROM COMENTARIO WHERE id_comentario = ? LIMIT 1', [comentarioId], (error) => {
+            const email = corregirFormato(req.params.email);
+            const titulo = corregirFormato(req.params.titulo);
+            const { comentario } = req.body;
+    
+            pool.query(`
+                DELETE FROM proyecto1.COMENTARIO
+                WHERE comentario = ? AND usuario = (
+                    SELECT id_usuario
+                    FROM proyecto1.USUARIO
+                    WHERE email = ?
+                ) AND libro = (
+                    SELECT id_libro
+                    FROM proyecto1.LIBRO
+                    WHERE titulo = ?
+                )`,
+                [comentario, email, titulo],
+                (error) => {
+                    if (error) {
+                        console.error('Error al eliminar el comentario:', error);
+                        res.status(500).json({ message: 'Error al eliminar el comentario' });
+                    } else {
                         res.status(200).json({ message: 'Comentario eliminado exitosamente' });
-                    });
-                });
-            });
+                    }
+                }
+            );
         } catch (error) {
             console.error('Error general al eliminar el comentario:', error);
             res.status(500).json({ message: 'Error general al eliminar el comentario' });
         }
     }
-
+    
+    
 
 }
 
--- Backend/src/keys.ts
+++ Backend/src/keys.ts
@@ -2,7 +2,7 @@ export default{
     database : {
         host: 'localhost',
         user: 'root',
-        password: 'password',
+        password: '201901055',
         database: 'proyecto1'
     }
 }
